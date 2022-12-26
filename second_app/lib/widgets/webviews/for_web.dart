ad3e57b7065cfab00ab73965f94796d7d6d3b0bd
Артем Ильинский <42186657+ilart1991@users.noreply.github.com>
2022-12-26 22:56:03 +0300

    ok

--- second_app/lib/widgets/webviews/for_web.dart
+++ second_app/lib/widgets/webviews/for_web.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
+// ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 
 import 'package:second_app/my_home_page.dart';
@@ -7,7 +8,7 @@ import 'package:second_app/my_home_page.dart';
 Widget webView() => ForWeb();
 
 class ForWeb extends StatelessWidget {
-  ForWeb() {
+  ForWeb({super.key}) {
     // ignore: undefined_prefixed_name
     ui.platformViewRegistry.registerViewFactory('iframe', (int viewId) {
       var iframe = html.IFrameElement();
@@ -17,6 +18,6 @@ class ForWeb extends StatelessWidget {
   }
   @override
   Widget build(BuildContext context) {
-    return Container(child: HtmlElementView(viewType: 'iframe'));
+    return const HtmlElementView(viewType: 'iframe');
   }
 }
